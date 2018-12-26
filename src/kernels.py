b2993f3281460ff8d8da8ff5bd9fdc08a7127566
leo <leozeitler95@gmail.com>
2018-12-26 17:55:09 +0100

    Update: Refactorization and implementation of classification
    Code was refactored for reusability among the classes
    Extended init function of parent class to initialize all class vars at one point
    Started classification approach
    
    Classification approach implemented so far breaks after first outer loop.
    More investigation needed

--- src/kernels.py
+++ src/kernels.py
@@ -10,16 +10,16 @@ def linearKernel(x, y, *args):
     return np.dot(x.T, y)
 
 
-def polynomialKernel(x, y, p):
+def polynomialKernel(x, y, *args):
     """For curved decision boundaries, where p controls polynomials degree"""
-    return math.pow((np.dot(x.T, y) + 1), p)
+    return math.pow((np.dot(x.T, y) + 1), args[0])
 
 
-def RBFKernel(x, y, sig):
+def RBFKernel(x, y, *args):
     """Uses explicit euclidian distance between x and y.
     sig controls the smoothness of the boundary."""
     num = distance.euclidean(x, y)
-    denom = 2 * sig ** 2
+    denom = 2 * args[0] ** 2
 
     return math.exp(- num / denom)
 
--- src/rvm.py
+++ src/rvm.py
@@ -18,9 +18,9 @@ import time
 
 # Set a random seed
 np.random.seed(0)
+EPS = 10 **-6
 
-
-class RVM():
+class RVM:
     """Relevance Vector Machine class implementation based on Mike Tipping's
     The Relevance Vector Machine (2000)
 
@@ -28,14 +28,15 @@ class RVM():
 
     def __init__(
             self,
-            kernelName='RBFKernel',
+            X,
+            T,
+            kernelName,
             p=3,
             sigma=5,
             alpha=10**-6,
             beta=10**-6,
             convergenceThresh=10**-3,
             alphaThresh=10**9,
-            maxIter = 25
             ):
         """RVM parameters initialization
 
@@ -51,16 +52,51 @@ class RVM():
                        in RVM classification
 
         """
+
         self.kernelName = kernelName
         self.p = p
         self.sigma = sigma
-        self.alpha = alpha
+
+        self.X = X
+        self.T = T
+        self.N = np.shape(X)[0]
+        self.phi = self._initPhi(X)
+        # Why assigning to the relevance vectors additionally 1?
+        # self.relevanceVectors = np.append(1, X)
+        self.relevanceVectors = X
+
         self.beta = beta
         self.convergenceThresh = convergenceThresh
         self.alphaThresh = alphaThresh
 
+        self.alpha = alpha * np.ones(self.N + 1)
 
-    def _initPhi(self, X, kernelName):
+        self.covPosterior = np.linalg.inv(
+            np.diag(self.alpha) + self.beta * np.dot(self.phi.T, self.phi))
+        self.muPosterior = self.beta * \
+                           np.dot(np.dot(self.covPosterior, self.phi.T), self.T)
+
+        # Why re assigning alpha?
+        self.alpha = -1 * np.ones(self.N + 1)
+
+    def _get_kernel_function(self):
+        if self.kernelName == 'linearKernel':
+            return linearKernel, None
+
+        elif self.kernelName == 'RBFKernel':
+            return RBFKernel, self.sigma
+
+        elif self.kernelName == 'polynomialKernel':
+            return polynomialKernel, self.p
+
+    def rvm_output(self, unseen_x):
+        kernel, args = self._get_kernel_function()
+        kernel_output = [kernel(unseen_x, x_i, args) for x_i in self.relevanceVectors]
+        kernel_output = np.insert(kernel_output, 0, 1)
+
+        return np.asarray(kernel_output).dot(self.muPosterior)
+
+    def _initPhi(self, X):
         """Initialize the design matrix based on the specified kernel function.
 
         Args:
@@ -74,44 +110,33 @@ class RVM():
         N = np.shape(X)[0]
         PHI = np.ones(N * (N+1)).reshape((N, N+1))
 
-        if self.kernelName == 'linearKernel':
-            for num, x_n in enumerate(X):
-                phi_x_n = [linearKernel(x_n, x_i) for x_i in X]
-                phi_x_n = np.insert(phi_x_n, 0, 1)
-                PHI[num] = phi_x_n
+        kernel, args = self._get_kernel_function()
 
-        if self.kernelName == 'RBFKernel':
-            for num, x_n in enumerate(X):
-                phi_x_n = [RBFKernel(x_n, x_i, self.sigma) for x_i in X]
-                phi_x_n = np.insert(phi_x_n, 0, 1)
-                PHI[num] = phi_x_n
-
-        if self.kernelName == 'polynomialKernel':
-            for num, x_n in enumerate(X):
-                phi_x_n = [polynomialKernel(x_n, x_i, self.p) for x_i in X]
-                phi_x_n = np.insert(phi_x_n, 0, 1)
-                PHI[num] = phi_x_n
+        for num, x_n in enumerate(X):
+            phi_x_n = [kernel(x_n, x_i, args) for x_i in X]
+            phi_x_n = np.insert(phi_x_n, 0, 1)
+            PHI[num] = phi_x_n
 
         return PHI
 
+    def _posterior(self):
+        pass
 
     def _prune(self):
         """Prunes alpha such that only relevant weights are kept"""
         useful = self.alpha < self.alphaThresh
         self.alpha = self.alpha[useful]
+
+        # Shouldn't it be for both dimensions?
         self.phi = self.phi[:, useful]
-        self.alphaOld = self.alphaOld[useful]
-        self.gamma = self.gamma[useful]
         self.covPosterior = self.covPosterior[np.ix_(useful, useful)]
         self.muPosterior = self.muPosterior[useful]
         self.relevanceVectors = self.relevanceVectors[useful]
 
-
-    def fit(self, X, T, kernelName):
+    def fit(self):
         """Fit the training data
 
         Args:
-        X (numpy.ndarray): the training data
         T (numpy.ndarray): the targets
         kernelName (str): the kernel function
 
@@ -119,37 +144,20 @@ class RVM():
         self (object): the RVM model with all its properties
 
         """
+        alphaOld = 0 * np.ones(self.N+1)
 
-        N = np.shape(X)[0]
-        self.phi = self._initPhi(X, kernelName)
-        self.relevanceVectors = np.append(1, X)
-        self.T = T
+        while abs(sum(self.alpha) - sum(alphaOld)) >= self.convergenceThresh:
+            alphaOld = np.array(self.alpha)
 
-        self.alpha = self.alpha * np.ones(N+1)
+            gamma = 1 - self.alpha * np.diag(self.covPosterior)
+            self.alpha = gamma / self.muPosterior**2
 
-        self.covPosterior = np.linalg.inv(
-                np.diag(self.alpha) + self.beta * np.dot(self.phi.T, self.phi))
-        self.muPosterior = self.beta * \
-                np.dot(np.dot(self.covPosterior, self.phi.T), self.T)
-
-        self.alphaOld = 0 * np.ones(N+1)
-        self.alpha = -1 * np.ones(N+1)
-
-        while abs(sum(self.alpha) - sum(self.alphaOld)) >= self.convergenceThresh:
-            self.alphaOld = np.array(self.alpha)
-
-            self.gamma = 1 - self.alpha * np.diag(self.covPosterior)
-            self.alpha = self.gamma / self.muPosterior**2
-
-            self.beta = (N - np.sum(self.gamma)) / \
-                    np.linalg.norm(T - np.dot(self.phi, self.muPosterior))**2
-
+            self.beta = (self.N - np.sum(gamma)) / \
+                    np.linalg.norm(self.T - np.dot(self.phi, self.muPosterior))**2
 
             self._prune()
 
             self._posterior()
-
-        return self
 
 
 class RVR(RVM):
@@ -161,120 +169,142 @@ class RVR(RVM):
         self.muPosterior = self.beta * \
                 np.dot(np.dot(self.covPosterior, self.phi.T), self.T)
 
-
-    def predict(self, X):
+    def predict(self, unseen_x):
         """Predict the value of a new data point
 
         Args:
-        X (numpy.ndarray): training data
+        unseen x (float): unseen data point
 
         Returns:
         T (numpy.ndarray): predicted target values
 
         """
-        phi = self.initPhi(X)
-        T = np.dot(phi, self.muPosterior)
-
-        return T
+        return self.rvm_output(unseen_x)
 
 
 class RVC(RVM):
     """Relevance Vector Machine classification"""
 
-    def _classify(self, weights, PHI):
-        """Classify a data point
+    def irls(self):
+        a = np.diag(self.alpha)
+        weight_old = self.muPosterior.copy()
+        weights_new = np.full(self.muPosterior.shape, np.inf)
+
+        counter =0
+        while np.absolute(weights_new - weight_old).all() >= EPS:
+            weight_old = weights_new.copy()
+            recent_likelihood, sigmoid = self._likelihood()
+            recent_likelihood_matrix = np.diag(recent_likelihood)
+            second_derivative = -(np.dot(self.phi.transpose().dot(recent_likelihood_matrix), self.phi) + a)
+            first_derivative = self.phi.transpose().dot(self.T - sigmoid) - a.dot(self.muPosterior)
+
+            weights_new = self.muPosterior - np.linalg.inv(second_derivative).dot(first_derivative)
+            self.covPosterior = np.linalg.inv(-second_derivative)
+            self.muPosterior = weights_new.copy()
+
+            counter+=1
+            print counter
+
+    def _likelihood(self):
+        """
+        Classify a data point
 
         Args:
-        weights (numpy.ndarray): weights corresponding to data point to be
-                                      classified
-        PHI (numpy.ndarray): design matrix
+        X (numpy.ndarray): datapoints
 
         Returns:
-        sigmoid (float): the sigmoid of the dot product of the weights with the
+        sigmoid (numpy.ndarray): the sigmoid of the dot product of the weights with the
                          design matrix
 
         """
 
-        dot = np.dot(weights.T, PHI)
-        sigmoid = 1 / (1 + math.exp(-dot))
+        sigmoid = np.asarray([ 1 / (1 + math.exp(-self.rvm_output(x))) for x in self.X])
+        beta = np.multiply(sigmoid, np.ones(sigmoid.shape) - sigmoid)
+        return beta, sigmoid
 
-        return sigmoid
+    # def _negativeLogPosterior(self, weights, PHI, alpha):
+    #     """Compute the negative log posterior
+    #
+    #     Args:
+    #     weights (numpy.ndarray):
+    #     PHI (numpy.ndarray): design matrix
+    #     alpha (numpy.ndarray):
+    #
+    #     Returns:
+    #     negativeLogPosterior (float): the negative log posterior
+    #     TODO Jacobian?
+    #
+    #     """
+    #
+    #     y = self._classify(weights, PHI)
+    #
+    #     termA = 0
+    #
+    #     A = np.diag(alpha)
+    #     termB = 0.5 * np.dot(weights.T, np.dot(A, weights))
+    #
+    #
+    #     negLogPost = termA + termB
+    #
+    #     return negLogPost#, jacobian?
+    #
+    # def _hessian(self, weights, alpha, PHI):
+    #     """Compute the Hessian matrix of the negative log posterior
+    #
+    #     Args:
+    #     weights (numpy.ndarray):
+    #     PHI (numpy.ndarray): design matrix
+    #     alpha (numpy.ndarray):
+    #
+    #     Returns:
+    #     hessian (numpy.ndarray): the Hessian matrix of the negative log posterior
+    #
+    #     """
+    #
+    #     y = self._classify(weights, PHI)
+    #
+    #     A = np.diag(alpha)
+    #     B = np.diag(y * (1-y))
+    #
+    #     hessian = np.dot(PHI.T, np.dot(B, PHI)) + A
+    #
+    #     return hessian
+    #
+    # def _findMode(self):
+    #     mode = minimize(
+    #             fun=self._negativeLogPosterior,
+    #             hess=self._hessian,
+    #             x0=self.weights,
+    #             args=(self.alpha, self.phi, self.c), # self.c needs to be defined somewhere
+    #             method='Newton-CG',
+    #             jac=True,
+    #             options={'maxiter': self.maxIter}
+    #             )
+    #
+    #     self.muPosterior = mode.x
+    #     self.covPosterior = np.linalg.inv(
+    #             self._hessian(self.muPosterior, self.alpha, self.phi, self.c)
+    #             )
 
+    def fit(self):
+        alphaOld = 0 * np.ones(self.N + 1)
 
-    def _negativeLogPosterior(self, weights, PHI, alpha):
-        """Compute the negative log posterior
+        while abs(sum(self.alpha) - sum(alphaOld)) >= self.convergenceThresh:
+            alphaOld = np.array(self.alpha)
 
-        Args:
-        weights (numpy.ndarray):
-        PHI (numpy.ndarray): design matrix
-        alpha (numpy.ndarray):
+            self.irls()
 
-        Returns:
-        negativeLogPosterior (float): the negative log posterior
-        TODO Jacobian?
+            gamma = 1 - self.alpha * np.diag(self.covPosterior)
+            self.alpha = gamma / self.muPosterior ** 2
 
-        """
+            self.beta = (self.N - np.sum(gamma)) / \
+                        np.linalg.norm(self.T - np.dot(self.phi, self.muPosterior)) ** 2
 
-        y = self._classify(weights, PHI)
+            # self._prune()
 
-        TODO
-        termA = 0
-
-        A = np.diag(alpha)
-        termB = 0.5 * np.dot(weights.T, np.dot(A, weights))
-
-
-        negLogPost = termA + termB
-
-        return negLogPost#, jacobian?
-
-
-    def _hessian(self, weights, alpha, PHI):
-        """Compute the Hessian matrix of the negative log posterior
-
-        Args:
-        weights (numpy.ndarray):
-        PHI (numpy.ndarray): design matrix
-        alpha (numpy.ndarray):
-
-        Returns:
-        hessian (numpy.ndarray): the Hessian matrix of the negative log posterior
-
-        """
-
-        y = self._classify(weights, PHI)
-
-        A = np.diag(alpha)
-        B = np.diag(y * (1-y))
-
-        hessian = np.dot(PHI.T, np.dot(B, PHI)) + A
-
-        return hessian
-
-
-    def _findMode(self):
-        mode = minimize(
-                fun=self._negativeLogPosterior,
-                hess=self._hessian,
-                x0=self.weights,
-                args=(self.alpha, self.phi, self.c), # self.c needs to be defined somewhere
-                method='Newton-CG',
-                jac=True,
-                options={'maxiter': self.maxIter}
-                )
-
-        self.muPosterior = mode.x
-        self.covPosterior = np.linalg.inv(
-                self._hessian(self.muPosterior, self.alpha, self.phi, self.c)
-                )
-
-
-    def fit(self, X, T):
-        """Fit the model"""
-
-        return 0
-
+            # self._posterior()
 
     def predict(self, X):
         """Make class predictions"""
+        pass
 

25c817a600d04f7b5d602bb8c98196e8bdbc4903
leo <leozeitler95@gmail.com>
2019-01-10 17:27:30 +0100

    Update: Added main classification file and made changes in rvc class
    Changed name of the call and changed data function

--- src/data.py
+++ src/data.py
@@ -1,24 +1,19 @@
-#!/usr/bin/env python3
-
 import numpy as np
 
 
-def createSimpleClassData(n, scale=10):
-    X = np.random.random(n) * scale - scale/2.
-    T = X > 0 #np.random.random(n) * 2 - 1 > 0
+def createSimpleClassData(n, w, scale=10):
+    X = np.random.rand(n, 2) * scale - scale/2.
+    T = X.dot(w) > 0
     return X, T.astype(int)
 
 
 def sinc(n, sigma):
     """Generate noisy or noise-free data from the sinc function f(x) = sin(x)/x
-
     Keyword arguments:
     n -- the number of of data points to be generated
     sigma -- noise variance
-
     """
     X = np.linspace(-10, 10, n)
     T = np.nan_to_num(np.sin(X)/X) + np.random.normal(0, sigma, n)
 
     return X, T
-
---
+++ src/main-classification.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+"""main.py: Relevance Vector Machine (RVM) for regression and classification."""
+
+__author__ = "Adrian Chiemelewski-Anders, Clara Tump, Bas Straathof \
+              and Leo Zeitler"
+
+from data import createSimpleClassData
+from rvm import RVR, RVC
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Set a random seed
+np.random.seed(0)
+
+
+def main():
+    N = 200
+    w = np.array([1, 1])
+    Xtrain, Ttrain = createSimpleClassData(N, w)
+    Xtest, Ttest = createSimpleClassData(int(N / 3), w)
+
+    clf = RVC(Xtrain, Ttrain, 'cosineKernel', alphaThresh=10e8, convergenceThresh=10e-4)
+    clf.fit()
+
+    print("The relevance vectors:")
+    print(clf.relevanceVectors)
+
+    # This is using training data -- should be changed of course
+    TPred = clf.predict(Xtest)
+
+    correct_classifications = Xtest.dot(w) > 0
+    pos_data = Xtest[correct_classifications == True]
+    neg_data = Xtest[correct_classifications == False]
+
+    plt.figure()
+    plt.scatter(pos_data[:, 0], pos_data[:, 1], label='Positive Data')
+    plt.scatter(neg_data[:, 0], neg_data[:, 1], label='Negative Data')
+
+    plt.xlabel("x")
+    plt.ylabel("t")
+    plt.legend()
+    plt.show()
+
+
+    plt.figure()
+
+    prediction_classification = TPred > 0.5
+    pos_data = Xtest[prediction_classification == True]
+    neg_data = Xtest[prediction_classification == False]
+
+    pos_sigmas = TPred[prediction_classification == True]
+    neg_sigmas = TPred[prediction_classification == False]
+
+    plt.scatter(pos_data[:, 0], pos_data[:, 1], s=pos_sigmas ** 2 * 50, label='Positive Data')
+    plt.scatter(neg_data[:, 0], neg_data[:, 1], s=(1-neg_sigmas) ** 2 *50, label='Negative Data')
+
+    plt.xlabel("foobar")
+    plt.ylabel("t")
+    plt.legend()
+    plt.show()
+
+    print(correct_classifications)
+    print(prediction_classification)
+
+    print('accuracy', (correct_classifications == prediction_classification).sum() / correct_classifications.shape[0])
+
+    print('alpha shape', clf.alpha.shape)
+    print(clf.alpha)
+
+if __name__ == '__main__':
+    main()

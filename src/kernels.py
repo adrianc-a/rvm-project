40d2d8159af2fbffbc2915d8a59b07e77de59e69
Bas Straathof <bas_straathof@msn.com>
2019-01-10 14:16:56 +0100

    Add linearSplineKernel and improve docstrings

--- src/kernels.py
+++ src/kernels.py
@@ -15,32 +15,48 @@ def linearKernel(x, y, *args):
     """Kernel for linear separation
 
     Args:
-    x (numpy.ndarray): a vector of datapoints
-    y (numpy.ndarray): a vector of datapoints
+    x (float): a datapoint
+    y (float): a datapoint
     *args (none)
 
     """
-    return np.dot(x.T, y)
+    return x.T * y
+
+
+def linearSplineKernel(x, y, *args):
+    """Univariate linear spline kernel
+
+    Args:
+    x (float): a datapoint
+    y (float): a datapoint
+    *args (none)
+
+    """
+    return 1 \
+           + x * y \
+           + x * y * min(x, y) \
+           - (x + y) / 2 * min(x, y)**2 \
+           + min(x, y)**3 / 3
 
 
 def polynomialKernel(x, y, *args):
     """Polynomial kernel for curved decision boundaries
 
     Args:
-    x (numpy.ndarray): a vector of datapoints
-    y (numpy.ndarray): a vector of datapoints
+    x (float): a datapoint
+    y (float): a datapoint
     *args (int): the degree of the polynomial
 
     """
-    return math.pow((np.dot(x.T, y) + 1), args[0])
+    return math.pow(x.T * y + 1, args[0])
 
 
 def RBFKernel(x, y, *args):
     """RBF kernel that uses explicit euclidian distance between x and y,
 
     Args:
-    x (numpy.ndarray): a vector of datapoints
-    y (numpy.ndarray): a vector of datapoints
+    x (float): a datapoint
+    y (float): a datapoint
     *args (float): sigma; controls the smoothness of the boundary
 
     """

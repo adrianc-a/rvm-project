578cfe3085119d8a77d223b13f63b1d81fc69f0e
Bas Straathof <bas_straathof@msn.com>
2019-01-10 14:33:41 +0100

    Improve structure and data loading of main.py
    Note that the current version is adjusted to the sinc data set for
    regression. This can be changed at leisure.

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 __author__ = "Adrian Chiemelewski-Anders, Clara Tump, Bas Straathof \
               and Leo Zeitler"
 
-from data import sincNoiseFree, createSimpleClassData
+from data import sinc, createSimpleClassData
 from rvm import RVR, RVC
 
 from sklearn.model_selection import train_test_split
@@ -15,42 +15,52 @@ import matplotlib.pyplot as plt
 # Set a random seed
 np.random.seed(0)
 
-def initData(N, dataset):
+
+def initData(N, dataset, *args):
     """Initialize the data set traning and testing examples"""
-    X, T = dataset(N)
+    X, T = dataset(N, *args)
 
     X_train, X_test, T_train, T_test = train_test_split(
-        X, T, test_size=0.2, random_state=42)
+            X, T, test_size=0.2, random_state=42)
 
     return X_train, X_test, T_train, T_test
 
 
 def main():
-    N = 200
-    X_train, X_test, T_train, T_test = initData(N, sincNoiseFree)
+    N = 100 # number of data points
+    noiseVariance = 0.01**2
+    dataFunction = sinc
 
-    clf = RVR(X_train, T_train, 'RBFKernel')
+    X_train, X_test, T_train, T_test = initData(N, sinc, noiseVariance)
+
+    clf = RVR(X_train, T_train, 'linearSplineKernel')
     clf.fit()
 
     print("The relevance vectors:")
     print(clf.relevanceVectors)
-    print(clf.T)
 
     # This is using training data -- should be changed of course
-    T_pred = clf.predict(X_train)
+    T_pred = clf.predict(X_test)
 
     # Plot training data
-    plt.scatter(X_train, T_train, label='Training Data')
+    plt.scatter(X_train, T_train, s=20, label='Training data')
 
     # Plot predictions
-    plt.scatter(X_train, T_pred, color='r', label='Predictions')
+    plt.scatter(X_test, T_pred, s=20, color='r', label='Predictions')
 
     # Plot relevance vectors
-    plt.scatter(clf.relevanceVectors, clf.T, s=50, facecolors="none", color="k", zorder=1)
+    plt.scatter(clf.relevanceVectors,
+                clf.T,
+                label="Relevance vectors",
+                s=50,
+                facecolors="none",
+                color="k",
+                zorder=1)
 
     plt.xlabel("x")
     plt.ylabel("t")
     plt.legend()
+    plt.savefig("../plots/sincdataplot.png", bbox_inches="tight")
     plt.show()
 
 

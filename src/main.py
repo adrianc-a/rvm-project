40b2ec76f6a6c00c362c18c85bf8627c2a3644e8
clara2911 <clara.tump@hotmail.com>
2019-01-11 10:17:45 +0100

    plotting of airfoil data

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@
 __author__ = "Adrian Chiemelewski-Anders, Clara Tump, Bas Straathof \
               and Leo Zeitler"
 
-from data import sinc, createSimpleClassData
+from data import sinc, createSimpleClassData, airfoil
 from rvm import RVR, RVC
 
 from sklearn.model_selection import train_test_split
@@ -15,10 +15,18 @@ import matplotlib.pyplot as plt
 # Set a random seed
 np.random.seed(0)
 
+# Proposal:
+# Accuracy measure: mean+std difference from true value
+# Plot: per dimension and one pca one
+#
+#
+#
+#
 
-def initData(N, dataset, *args):
+def initData(N, dataset): #*args
     """Initialize the data set traning and testing examples"""
-    X, T = dataset(N, *args)
+    # X, T = dataset(N, *args)
+    X, T = dataset(N)
 
     X_train, X_test, T_train, T_test = train_test_split(
             X, T, test_size=0.2, random_state=42)
@@ -27,29 +35,31 @@ def initData(N, dataset, *args):
 
 
 def main():
-    N = 100 # number of data points
+    N = 1000 # number of data points
     noiseVariance = 0.01**2
     dataFunction = sinc
 
-    X_train, X_test, T_train, T_test = initData(N, sinc, noiseVariance)
-
-    clf = RVR(X_train, T_train, 'linearSplineKernel')
+    # X_train, X_test, T_train, T_test = initData(N, sinc, noiseVariance)
+    X_train, X_test, T_train, T_test = initData(N, airfoil)
+    print("train data ", X_train)
+    clf = RVR(X_train, T_train, 'RBFKernel')
     clf.fit()
 
-    print("The relevance vectors:")
-    print(clf.relevanceVectors)
+    print("Number of relevance vectors:")
+    print(len(clf.relevanceVectors))
 
     # This is using training data -- should be changed of course
-    T_pred = clf.predict(X_test)
+    T_pred = clf.predict(X_train)
 
-    # Plot training data
-    plt.scatter(X_train, T_train, s=20, label='Training data')
+    for i in range(5):
+        # Plot training data
+        plt.scatter(X_train[:,i], T_train, s=20, label='Training data')
 
-    # Plot predictions
-    plt.scatter(X_test, T_pred, s=20, color='r', label='Predictions')
+        # Plot predictions
+        plt.scatter(X_train[:,i], T_pred, s=20, color='r', label='Predictions')
 
-    # Plot relevance vectors
-    plt.scatter(clf.relevanceVectors,
+        # Plot relevance vectors
+        plt.scatter(clf.relevanceVectors[:,i],
                 clf.T,
                 label="Relevance vectors",
                 s=50,
@@ -57,11 +67,11 @@ def main():
                 color="k",
                 zorder=1)
 
-    plt.xlabel("x")
-    plt.ylabel("t")
-    plt.legend()
-    # plt.savefig("../plots/sincdataplot.png", bbox_inches="tight")
-    plt.show()
+        plt.xlabel("x")
+        plt.ylabel("t")
+        plt.legend()
+        # plt.savefig("../plots/sincdataplot.png", bbox_inches="tight")
+        plt.show()
 
 
 if __name__ == '__main__':
